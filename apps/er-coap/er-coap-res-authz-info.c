4d8635c7035eb3fd5dbd0a1a9c5eec051e97e934
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2018-10-21 14:14:34 -0400

    Fixed response and return message handling.

--- apps/er-coap/er-coap-res-authz-info.c
+++ apps/er-coap/er-coap-res-authz-info.c
@@ -10,7 +10,6 @@
 #include <string.h>
 #include "rest-engine.h"
 #include "cfs/cfs.h"
-#include "er-coap-constants.h"
 #include "./cwt.h"
 
 static void res_post_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
@@ -20,10 +19,6 @@ RESOURCE(res_authz_info, NULL, NULL, res_post_handler, NULL, NULL);
 res_post_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset){
   const uint8_t *token = NULL;
   int n;
-  char const *const success_message = "ACE credentials added";
-  char const *const no_token_message = "No token was received";
-  char const *const failure_message = "Failed to add ACE credentials";
-  char* response_buffer;
 
   size_t token_len = REST.get_request_payload(request, (const uint8_t **)&token);
   printf("token_len is %d\n", token_len);
@@ -52,17 +47,21 @@ res_post_handler(void *request, void *response, uint8_t *buffer, uint16_t prefer
       }
       printf("\n");
     } else {
-      REST.set_response_status(response, INTERNAL_SERVER_ERROR_5_00);
-      memcpy(response_buffer, failure_message, strlen(failure_message));
+      REST.set_response_status(response, REST.status.INTERNAL_SERVER_ERROR);
+      const char* failure_message = "Failed to add ACE credentials";
+      REST.set_response_payload(response, failure_message, strlen(failure_message));
     }
 
-    REST.set_response_status(response, CREATED_2_01);
+    REST.set_response_status(response, REST.status.CREATED);
+    const char* success_message = "ACE credentials added";
+    REST.set_response_payload(response, success_message, strlen(success_message));
+
   }
   else {
-    REST.set_response_status(response, BAD_REQUEST_4_00);
-    memcpy(response_buffer, no_token_message, strlen(no_token_message));
+    REST.set_response_status(response, REST.status.BAD_REQUEST);
+    const char* no_token_message = "No token was received";
+    REST.set_response_payload(response, no_token_message, strlen(no_token_message));
   }
 
-  REST.set_header_content_type(response, REST.type.APPLICATION_OCTET_STREAM);
-  REST.set_response_payload(response, response_buffer, strlen(response_buffer));
+  REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
 }
