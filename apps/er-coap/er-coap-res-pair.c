09bb90c0dffc277302a1c8801a1adadaec8eff2c
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2018-10-21 17:19:42 -0400

    Started working on pairing code

--- apps/er-coap/er-coap-res-pair.c
+++ apps/er-coap/er-coap-res-pair.c
@@ -18,31 +18,39 @@ static void res_post_handler(void *request, void *response, uint8_t *buffer, uin
 RESOURCE(res_pair, NULL, NULL, res_post_handler, NULL, NULL);
 
 res_post_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset){
-  const uint8_t *aes_token = NULL;
   char* AS_key;
-  size_t len;
   char *token_file = "tokens";
-  int fd_write, n;
-  char const *const success_message = "AS credentials added";
-  char const *const failure_message = "Failed to add AS credentials";
+  int fd_write;
 
-  len = REST.get_request_payload(request, (const uint8_t **)&aes_token);
+  const uint8_t *pairing_info = NULL;
+  int len = REST.get_request_payload(request, (const uint8_t **)&pairing_info);
   printf("Len is %d\n", len);
-  printf("Token:");
-  int i;
-  for (i=0; i<len; i++){
-    printf(" %x",aes_token[i]);
-  }
-  printf("\n");
-  n = read_cbor(aes_token, len);
 
-  if(n == 0){
-    memcpy(buffer, success_message, strlen(success_message));
+  if(len > 0) {
+    printf("Pairing info:");
+    int i;
+    for (i=0; i<len; i++){
+      printf(" %x",pairing_info[i]);
+    }
+    printf("\n");
+
+    int n = read_cbor(pairing_info, len);
+    if(n == 0){
+      REST.set_response_status(response, REST.status.CREATED);
+      const char* success_message = "AS credentials added";
+      REST.set_response_payload(response, success_message, strlen(success_message));
+    } else {
+      REST.set_response_status(response, REST.status.INTERNAL_SERVER_ERROR);
+      const char* failure_message = "Failed to add AS credentials";
+      REST.set_response_payload(response, failure_message, strlen(failure_message));
+    }
   } else {
-    memcpy(buffer, failure_message, strlen(failure_message));
+    REST.set_response_status(response, REST.status.BAD_REQUEST);
+    const char* no_info_message = "No pairing info was received";
+    REST.set_response_payload(response, no_info_message, strlen(no_info_message));
   }
 
   REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
-  REST.set_response_payload(response, buffer, strlen(buffer));
+
 }
 
