b89eddccb305f001f3e5f64886695e2d0698c36e
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2018-10-21 10:26:43 -0400

    Better error handling, not tested

--- apps/er-coap/er-coap-res-authz-info.c
+++ apps/er-coap/er-coap-res-authz-info.c
@@ -10,6 +10,7 @@
 #include <string.h>
 #include "rest-engine.h"
 #include "cfs/cfs.h"
+#include "er-coap-constants.h"
 #include "./cwt.h"
 
 static void res_post_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
@@ -18,38 +19,50 @@ RESOURCE(res_authz_info, NULL, NULL, res_post_handler, NULL, NULL);
 
 res_post_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset){
   const uint8_t *token = NULL;
-  size_t len;
   int n;
   char const *const success_message = "ACE credentials added";
+  char const *const no_token_message = "No token was received";
   char const *const failure_message = "Failed to add ACE credentials";
+  char* response_buffer;
 
-  len = REST.get_request_payload(request, (const uint8_t **)&token);
-  printf("Len is %d\n", len);
-  printf("Token:");
-  int i;
-  for (i=0; i<114; i++){
-    printf(" %x",token[i]);
-  }
-  printf("\n");
-  n = read_cbor(token, 113); 
+  size_t token_len = REST.get_request_payload(request, (const uint8_t **)&token);
+  printf("token_len is %d\n", token_len);
 
-  char *token_file = "tokens";
-  int fd_read, o;
-
-  fd_read = cfs_open(token_file, CFS_READ);
-  if (fd_read != -1){
-    o = cfs_read(fd_read, buffer, 128);
-    cfs_close(fd_read);
-    printf("File:");
+  if(token_len > 0) {
+    printf("Token:");
     int i;
-    for (i=0; i<128; i++){
-      printf(" %x",buffer[i]);
+    for (i=0; i<token_len; i++){
+      printf(" %x",token[i]);
     }
     printf("\n");
-  } else {
-    memcpy(buffer, failure_message, strlen(failure_message));
+    n = read_cbor(token, token_len);
+
+    char *token_file = "tokens";
+    int fd_read;
+    int bytes_read;
+
+    fd_read = cfs_open(token_file, CFS_READ);
+    if (fd_read != -1){
+      bytes_read = cfs_read(fd_read, buffer, 128);
+      cfs_close(fd_read);
+      printf("File:");
+      int i;
+      for (i=0; i<128; i++){
+        printf(" %x",buffer[i]);
+      }
+      printf("\n");
+    } else {
+      REST.set_response_status(response, INTERNAL_SERVER_ERROR_5_00);
+      memcpy(response_buffer, failure_message, strlen(failure_message));
+    }
+
+    REST.set_response_status(response, CREATED_2_01);
+  }
+  else {
+    REST.set_response_status(response, BAD_REQUEST_4_00);
+    memcpy(response_buffer, no_token_message, strlen(no_token_message))
   }
 
   REST.set_header_content_type(response, REST.type.APPLICATION_OCTET_STREAM);
-  REST.set_response_payload(response, buffer, strlen(buffer));
+  REST.set_response_payload(response, response_buffer, strlen(response_buffer));
 }
