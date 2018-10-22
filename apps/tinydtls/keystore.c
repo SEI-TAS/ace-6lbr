8f2e08ec5b8a15567b545176445dcafe6c6acdd4
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2018-10-22 11:18:21 -0400

    Changed to have actual default pairing key.

--- apps/tinydtls/keystore.c
+++ apps/tinydtls/keystore.c
@@ -7,18 +7,29 @@
 #include "dtls.h"
 #include "dtls_debug.h"
 
-#define DEFAULT_KEY_SIZE 40
+#define KEY_SIZE_BYTES 16
+#define PAIRING_KEY_ID "Authentication01"
+#define PAIRING_KEY_TOKEN_LENGTH "0000"
+#define TOKEN_FILE "tokens"
 
 void keystore_init(){
   dtls_debug("Creating keystore...\n");
-  /* char line[32] = {0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x52, 0x53, 0x31, 0xa1, 0xa2, 0xa3, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10}; */
-  char line[DEFAULT_KEY_SIZE] = "AAAAAAAABBBBBBBBCCCCCCCCDDDDDDDD0004test";
-  char *token_file = "tokens";
-  int fd_write, n;
+  //char pairing_key[32] = {0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x52, 0x53, 0x31, 0xa1, 0xa2, 0xa3, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10};
+  char pairing_key[KEY_SIZE_BYTES] = {'a', 'b', 'c', 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+
+  const char* token_file = TOKEN_FILE;
+  int fd_write;
+  int bytes_written;
   fd_write = cfs_open(token_file, CFS_WRITE);
   if(fd_write != -1){
-    n = cfs_write(fd_write, line, DEFAULT_KEY_SIZE);
+    bytes_written = cfs_write(fd_write, PAIRING_KEY_ID, strlen(PAIRING_KEY_ID));
+    bytes_written = cfs_write(fd_write, pairing_key, KEY_SIZE_BYTES);
+    bytes_written = cfs_write(fd_write, PAIRING_KEY_TOKEN_LENGTH, strlen(PAIRING_KEY_TOKEN_LENGTH));
+    dtls_debug("Stored default pairing key in tokens file.\n");
     cfs_close(fd_write);
+  }
+  else {
+    dtls_debug("Could not open tokens file to initialize it with default pairing key.\n");
   }
 
 }
