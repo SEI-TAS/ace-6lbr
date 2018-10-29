755f291d3bf484c96bd14ffc4ae36b5fa29b86fe
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2018-10-29 11:36:10 -0400

    Typo

--- apps/er-coap/resources.h
+++ apps/er-coap/resources.h
@@ -5,4 +5,4 @@
 #define SCOPES "HelloWorld;rw_Lock;r_Lock"
 
 void find_dtls_context_key_id();
-int can_access_resource(const char* resource, rest_resource_flags_t method, unsigned char* key_id, int key_id_len) {
+int can_access_resource(const char* resource, rest_resource_flags_t method, unsigned char* key_id, int key_id_len);
