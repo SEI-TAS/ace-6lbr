1831d73765323de263bbe9a1d3e1bbfb7983cfaf
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2018-10-29 12:28:52 -0400

    Peer navigating

--- apps/er-coap/dtls_helpers.c
+++ apps/er-coap/dtls_helpers.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "dtls.h"
+#include "peer.h"
 #include "dtls_crypto.h"
 
 #include "cwt.h"
@@ -36,18 +37,22 @@ int lookup_dtls_key(const unsigned char * const id, size_t id_len,
 void find_dtls_context_key_id(context_t* ctx) {
     // Identity is in ctx->peers[0?]->handshake_parameters->keyx.psk.identity
 
-    if(ctx->peers) {
+    dtls_peer_t* curr_peer = ctx->peers;
+    while(curr_peer) {
       printf("Checking peer info: Peer role: %d\n", ctx->peers->role);
       dtls_handshake_parameters_t* params = ctx->peers->handshake_params;
       if(params == 0) {
         printf("No handshake params found!\n");
-        return;
+        curr_peer = curr_peer->next;
+        continue;
       }
       printf("Identity length: %d\n", params->keyx.psk.id_length);
       if(params->keyx.psk.id_length > 0) {
         printf("Identity: \n");
         HEX_PRINTF(ctx->peers->handshake_params->keyx.psk.identity, ctx->peers->handshake_params->keyx.psk.id_length);
       }
+
+      curr_peer = curr_peer->next;
     }
 }
 
