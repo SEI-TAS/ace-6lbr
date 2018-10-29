497b1c9cc0d0842ca0b764543f414d33cd38715b
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2018-10-29 11:41:25 -0400

    Function header consistency

--- apps/er-coap/resources.h
+++ apps/er-coap/resources.h
@@ -9,7 +9,7 @@
 // TODO: fix this, this is NOT extensible to add more resources.
 #define SCOPES "HelloWorld;rw_Lock;r_Lock"
 
-void find_dtls_context_key_id();
+void find_dtls_context_key_id(context_t* ctx);
 int can_access_resource(const char* resource, rest_resource_flags_t method, unsigned char* key_id, int key_id_len);
 
 #endif // RESOURCES_H
