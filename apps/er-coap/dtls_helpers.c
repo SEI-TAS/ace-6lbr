977a16cafb5a00e6aca2e9e138c8725dc16bc15d
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2018-10-29 12:19:56 -0400

    Debugging key access

--- apps/er-coap/dtls_helpers.c
+++ apps/er-coap/dtls_helpers.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "dtls.h"
+#include "dtls_crypto.h"
 
 #include "cwt.h"
 #include "dtls_helpers.h"
@@ -37,8 +38,16 @@ void find_dtls_context_key_id(context_t* ctx) {
 
     if(ctx->peers) {
       printf("Checking peer info: Peer role: %d\n", ctx->peers->role);
-      printf("Identity: ");
-      HEX_PRINTF(ctx->peers->handshake_params->keyx.psk.identity, ctx->peers->handshake_params->keyx.psk.id_length);
+      dtls_handshake_parameters_t params* = ctx->peers->handshake_params;
+      if(params == 0) {
+        printf("No handshake params found!\n");
+        return;
+      }
+      printf("Identity length: %d\n", params->keyx.psk.id_length);
+      if(params->keyx.psk_id_length > 0) {
+        printf("Identity: \n");
+        HEX_PRINTF(ctx->peers->handshake_params->keyx.psk.identity, ctx->peers->handshake_params->keyx.psk.id_length);
+      }
     }
 }
 
