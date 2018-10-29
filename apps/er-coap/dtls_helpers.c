f5704feb7902a03b67d236b5f85aafc6430b6178
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2018-10-29 12:48:24 -0400

    Using static global variable...

--- apps/er-coap/dtls_helpers.c
+++ apps/er-coap/dtls_helpers.c
@@ -11,6 +11,9 @@
 #include "key-token-store.h"
 #include "utils.h"
 
+static unsigned char* current_key_id = 0;
+static int current_key_length;
+
 int lookup_dtls_key(const unsigned char * const id, size_t id_len,
          unsigned char * const result, size_t result_length){
 
@@ -24,6 +27,13 @@ int lookup_dtls_key(const unsigned char * const id, size_t id_len,
     else {
       memcpy(result, tok.key, KEY_LENGTH);
       key_length = KEY_LENGTH;
+
+      if(current_key_id) {
+        free(current_key_id);
+      }
+      current_key_length = id_len;
+      current_key_id = (unsigned char*) malloc(id_len);
+      memcpy(current_key_id, id, id_len);
     }
   }
   else {
@@ -36,10 +46,12 @@ int lookup_dtls_key(const unsigned char * const id, size_t id_len,
 //
 void find_dtls_context_key_id(context_t* ctx) {
     // Identity is in ctx->peers[0?]->handshake_parameters->keyx.psk.identity
+    printf("Last stored key id: ");
+    HEX_PRINTF(current_key_id, current_key_length);
 
     dtls_peer_t* curr_peer = ctx->peers;
     while(curr_peer) {
-      printf("Checking peer info: Peer role: %d\n", ctx->peers->role);
+      printf("Checking peer info: Peer role: %d; connection state: %d\n", ctx->peers->role, ctx->peers->state);
       dtls_handshake_parameters_t* params = ctx->peers->handshake_params;
       if(params == 0) {
         printf("No handshake params found! Trying next peer.\n");
