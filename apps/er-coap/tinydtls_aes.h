6b53195d9856c336572b299579910a521db38cff
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2018-11-05 11:09:54 -0500

    Another missing comma

--- apps/er-coap/tinydtls_aes.h
+++ apps/er-coap/tinydtls_aes.h
@@ -4,7 +4,7 @@
 int
 dtls_decrypt_with_nounce_len(const unsigned char *src, size_t length,
 	     unsigned char *buf,
-	     unsigned char *nounce, size_t nounce_len
+	     unsigned char *nounce, size_t nounce_len,
 	     unsigned char *key, size_t keylen);
 
 #endif // TINYDTLS_AES_H
