9ff62cd225d0b598f3bfecab5704b69d75dd9608
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-01-11 18:04:04 -0500

    First attempt to add process with timer to revocation check.

--- apps/er-coap/revocation.c
+++ apps/er-coap/revocation.c
@@ -17,21 +17,43 @@ DM18-1273
 #include <string.h>
 
 #include "cfs/cfs.h"
+#include "sys/etimer.h"
 
 #include "key-token-store.h"
 #include "resources.h"
 #include "cwt.h"
 #include "utils.h"
 
-// 0. Figure out how to store the IP of each AS associated with the token. When pairing?
+// 0. DONE! Figure out how to store the IP of each AS associated with the token. When pairing?
 // 1. Timed process or sleep.
 // 2. CoAP client to send request.
 // 3. DONE! Loop over all tokens.
 // 4. Parse revocation response.
 // 5. DONE! Remove revoked tokens froms storage.
 
+#define CHECK_WAIT_TIME_SECS 20
+
+void check_revoked_tokens();
+
+/*---------------------------------------------------------------------------*/
+PROCESS(revocation_check, "Revoked Tokens Checker");
+AUTOSTART_PROCESSES(&revocation_check);
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(acers, ev, data)
+{
+  PROCESS_BEGIN();
+
+  check_revoked_tokens();
+
+  PROCESS_END();
+}
+
+// Main function to check for revoked tokens.
 void check_revoked_tokens() {
-  printf("Revoked tokens checker!\n");
+  static struct etimer et = 0;
+  printf("Executing revoked tokens checker!\n");
 
   // First get the AS IP.
   authz_entry as_pairing_entry = { 0 };
@@ -45,48 +67,66 @@ void check_revoked_tokens() {
   PRINTIP6ADDR(as_pairing_entry.claims);
   printf("\n");
 
-  authz_entry_iterator iterator = authz_entry_iterator_initialize();
+  while(1) {
+      printf("Executing check iteration.");
 
-  // Add all revoked tokens to a list (removing them all together later is more efficient).
-  printf("Looping over all tokens to find revoked ones.\n");
-  authz_entry* curr_entry = authz_entry_iterator_get_next(&iterator);
-  int num_tokens_to_remove = 0;
-  authz_entry* tokens_to_remove[MAX_AUTHZ_ENTRIES] = {0};
-  while(curr_entry != 0) {
-    printf("Curr entry kid: ");
-    HEX_PRINTF(curr_entry->kid, KEY_ID_LENGTH);
+      authz_entry_iterator iterator = authz_entry_iterator_initialize();
 
-    int token_was_revoked = 0;
-    // 2. CoAP client to send request.
-    // 4. Parse revocation response.
+      // Add all revoked tokens to a list (removing them all together later is more efficient).
+      printf("Looping over all tokens to find revoked ones.\n");
+      authz_entry* curr_entry = authz_entry_iterator_get_next(&iterator);
+      int num_tokens_to_remove = 0;
+      authz_entry* tokens_to_remove[MAX_AUTHZ_ENTRIES] = {0};
+      while(curr_entry != 0) {
+        printf("Curr entry kid: ");
+        HEX_PRINTF(curr_entry->kid, KEY_ID_LENGTH);
 
-    // Add token to removal list if revoked, or free its temp memory if not.
-    if(token_was_revoked) {
-      printf("Adding token to removal list.\n");
-      tokens_to_remove[num_tokens_to_remove++] = curr_entry;
-    }
-    else {
-      free_authz_entry(curr_entry);
-      free(curr_entry);
-    }
+        int token_was_revoked = 0;
+        // 2. CoAP client to send request.
+        // 4. Parse revocation response.
 
-    curr_entry = authz_entry_iterator_get_next(&iterator);
-  }
+        // Add token to removal list if revoked, or free its temp memory if not.
+        if(token_was_revoked) {
+          printf("Adding token to removal list.\n");
+          tokens_to_remove[num_tokens_to_remove++] = curr_entry;
+        }
+        else {
+          free_authz_entry(curr_entry);
+          free(curr_entry);
+        }
 
-  authz_entry_iterator_finish(iterator);
+        curr_entry = authz_entry_iterator_get_next(&iterator);
+      }
 
-  // Remove all revoked tokens, and then free the memory for their temp structs.
-  printf("Total tokens to remove: %d\n", num_tokens_to_remove);
-  if(num_tokens_to_remove > 0) {
-    int num_removed = remove_authz_entries(tokens_to_remove, num_tokens_to_remove);
-    printf("Removed %d tokens.\n", num_removed);
+      authz_entry_iterator_finish(iterator);
 
-    int i = 0;
-    for(i = 0; i < num_tokens_to_remove; i++) {
-      authz_entry* curr_entry = tokens_to_remove[i];
-      free_authz_entry(curr_entry);
-      free(curr_entry);
-    }
+      // Remove all revoked tokens, and then free the memory for their temp structs.
+      printf("Total tokens to remove: %d\n", num_tokens_to_remove);
+      if(num_tokens_to_remove > 0) {
+        int num_removed = remove_authz_entries(tokens_to_remove, num_tokens_to_remove);
+        printf("Removed %d tokens.\n", num_removed);
+
+        int i = 0;
+        for(i = 0; i < num_tokens_to_remove; i++) {
+          authz_entry* curr_entry = tokens_to_remove[i];
+          free_authz_entry(curr_entry);
+          free(curr_entry);
+        }
+      }
+      else {
+        printf("No tokens to remove.");
+      }
+
+      printf("Finished executing check iteration.");
+
+      // Set or reset timer and check again in a while.
+      if(et == 0) {
+        etimer_set(&et, CHECK_WAIT_TIME_SECS * CLOCK_SECOND);
+      }
+      else {
+        etimer_reset(&et);
+      }
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
   }
 }
 
