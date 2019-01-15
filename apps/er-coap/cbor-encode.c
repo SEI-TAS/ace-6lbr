055630d8a0c7747ab6c40bc87df3d41bb24e1257
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-01-15 15:05:04 -0500

    Finished function to encode a CBOR a map with only 1 pair, int-bytes.

--- apps/er-coap/cbor-encode.c
+++ apps/er-coap/cbor-encode.c
@@ -16,8 +16,6 @@ DM18-1273
 #include "utils.h"
 #include "cbor-encode.h"
 
-#define CBOR_PREFIX_MAP_HEADER_LENGTH 1
-
 #define DEBUG 0
 #if DEBUG
 #include <stdio.h>
@@ -28,25 +26,26 @@ DM18-1273
 #define HEX_PRINTF_INL(byte_array, length)
 #endif
 
-int encode_pair_to_cbor(int key, int int_value, const char* str_value, unsigned char** cbor_result);
+int encode_pair_to_cbor(int key, int int_value, const char* bytes_value, int bytes_value_len, unsigned char** cbor_result);
 
 // Encodes a map of 2 key-value pairs into CBOR. Keys are ints, values can be ints or strs.
 int encode_map_to_cbor(int key1, int int_value1, const char* str_value1,
                        int key2, int int_value2, const char* str_value2, unsigned char** cbor_result) {
   // Map will have 2 pairs.
-  unsigned char cbor_map_header = CBOR_PREFIX_MAP | 2;
+  int number_of_pairs = 2;
+  unsigned char cbor_map_header = CBOR_PREFIX_MAP | number_of_pairs;
   unsigned char* pair1_cbor = 0;
-  int pair1_len = encode_pair_to_cbor(key1, int_value1, str_value1, &pair1_cbor);
+  int pair1_len = encode_pair_to_cbor(key1, int_value1, str_value1, strlen(str_value1), &pair1_cbor);
   unsigned char* pair2_cbor = 0;
-  int pair2_len = encode_pair_to_cbor(key2, int_value2, str_value2, &pair2_cbor);
+  int pair2_len = encode_pair_to_cbor(key2, int_value2, str_value2, strlen(str_value2), &pair2_cbor);
 
   // Move both encoded bytes into a unified buffer.
-  int cbor_bytes_len = CBOR_PREFIX_MAP_HEADER_LENGTH + pair1_len + pair2_len;
+  int cbor_bytes_len = sizeof(cbor_map_header) + pair1_len + pair2_len;
   PRINTF("Encoding both pairs in map, total length %d.\n", cbor_bytes_len);
   *cbor_result = (unsigned char*) malloc(cbor_bytes_len);
   (*cbor_result)[0] = cbor_map_header;
-  memcpy((*cbor_result) + CBOR_PREFIX_MAP_HEADER_LENGTH, pair1_cbor, pair1_len);
-  memcpy((*cbor_result) + CBOR_PREFIX_MAP_HEADER_LENGTH + pair1_len, pair2_cbor, pair2_len);
+  memcpy((*cbor_result) + sizeof(cbor_map_header), pair1_cbor, pair1_len);
+  memcpy((*cbor_result) + sizeof(cbor_map_header) + pair1_len, pair2_cbor, pair2_len);
   free(pair1_cbor);
   free(pair2_cbor);
   PRINTF("Final encoded bytes: ");
@@ -55,8 +54,29 @@ int encode_map_to_cbor(int key1, int int_value1, const char* str_value1,
   return cbor_bytes_len;
 }
 
+// Encodes a map of 1 key-value pair into CBOR. Key is int, value is a byte array.
 // NOTE: We assume all ints, keys or values, will be less than 24, to simplify encoding.
-int encode_pair_to_cbor(int key, int int_value, const char* str_value, unsigned char** cbor_result) {
+int encode_single_pair_to_cbor_map(int key, const unsigned char* value, int value_len, unsigned char** cbor_result) {
+  int number_of_pairs = 1;
+  unsigned char cbor_map_header = CBOR_PREFIX_MAP | number_of_pairs;
+
+  unsigned char* pair1_cbor = 0;
+  int pair1_len = encode_pair_to_cbor(key, 0, value, value_len, &pair1_cbor);
+
+  int cbor_bytes_len = sizeof(cbor_map_header) + pair1_len;
+  PRINTF("Encoding pair in map, total length %d.\n", cbor_bytes_len);
+  *cbor_result = (unsigned char*) malloc(cbor_bytes_len);
+  (*cbor_result)[0] = cbor_map_header;
+  memcpy((*cbor_result) + sizeof(cbor_map_header), pair1_cbor, pair1_len);
+  free(pair1_cbor);
+  PRINTF("Final encoded bytes: ");
+  HEX_PRINTF_INL((*cbor_result), cbor_bytes_len);
+
+  return cbor_bytes_len;
+}
+
+// NOTE: We assume all ints, keys or values, will be less than 24, to simplify encoding.
+int encode_pair_to_cbor(int key, int int_value, const char* bytes_value, int bytes_value_len, unsigned char** cbor_result) {
   PRINTF("Encoding pair to CBOR, with key %d, int value %d.\n", key, int_value);
 
   // Encode using the CBOR RFC rules. First key.
@@ -68,40 +88,12 @@ int encode_pair_to_cbor(int key, int int_value, const char* str_value, unsigned 
   PRINTF("Encoding value.\n");
   unsigned char* encoded_value;
   int encoded_value_len;
-  if(str_value != 0) {
-    encoded_value_len = encode_bytes_to_cbor(str_value, strlen(str_value), &encoded_value);
+  if(bytes_value != 0) {
+    encoded_value_len = encode_bytes_to_cbor(bytes_value, bytes_value_len, &encoded_value);
   }
   else {
     encoded_value_len = encode_int_to_cbor(int_value, &encoded_value);
   }
-
-  // Put key and value contigously in a buffer.
-  int encoded_len = encoded_key_len + encoded_value_len;
-  PRINTF("Encoding key and value together, total length %d.\n", encoded_len);
-  *cbor_result = (unsigned char*) malloc(encoded_len);
-  memcpy((*cbor_result), encoded_key, encoded_key_len);
-  memcpy((*cbor_result) + encoded_key_len, encoded_value, encoded_value_len);
-  free(encoded_key);
-  free(encoded_value);
-
-  PRINTF("Encoded bytes: ");
-  HEX_PRINTF_INL((*cbor_result), encoded_len);
-
-  return encoded_len;
-}
-
-// NOTE: We assume all ints, keys or values, will be less than 24, to simplify encoding.
-int encode_single_pair_to_cbor_map(int key, const unsigned char* value, int value_len, unsigned char** cbor_result) {
-  PRINTF("Encoding single pair to CBOR map, with key %d, int value: ");
-  HEX_PRINTF_INL(value, value_len);
-
-  PRINTF("Encoding key %d.\n", key);
-  unsigned char* encoded_key;
-  int encoded_key_len = encode_int_to_cbor(key, &encoded_key);
-
-  PRINTF("Encoding value.\n");
-  unsigned char* encoded_value;
-  int encoded_value_len = encode_bytes_to_cbor(value, strlen(value), &encoded_value);
 
   // Put key and value contigously in a buffer.
   int encoded_len = encoded_key_len + encoded_value_len;
