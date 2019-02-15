b1966aaadf609dee67820f915494e90315e65a3d
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-02-15 15:42:04 -0500

    More typos

--- apps/er-coap/key-token-store.c
+++ apps/er-coap/key-token-store.c
@@ -182,7 +182,7 @@ void initialize_key_token_store() {
 authz_entry* create_authz_entry(unsigned char* kid, int kid_len, unsigned char* key, int claims_len, unsigned char* claims, uint64_t time) {
   authz_entry* authz_info = (authz_entry*) malloc(sizeof(authz_entry));
   authz_info->kid = left_pad_array(kid, kid_len, KEY_ID_LENGTH, 0);
-  authz_info->key = (unsigned char*) malloc(key_len);
+  authz_info->key = (unsigned char*) malloc(KEY_LENGTH);
   memcpy(authz_info->key, key, KEY_LENGTH);
   authz_info->claims_len = claims_len;
   authz_info->claims = (unsigned char*) malloc(claims_len);
